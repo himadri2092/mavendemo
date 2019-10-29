dc0610017d7f164fe9daf4c1337bd4776cb2240e
himadri2092 <57128447+himadri2092@users.noreply.github.com>
2019-10-29 15:02:25 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -17,7 +17,7 @@ public class Calculator implements ICalculator {
 
 	public int divison(int a, int b) throws Exception {
 		if (b == 0) {
-			throw new Exception("Divider can't be zero");
+			throw new Exception("Divider can't be zero.try something else");
 		}
 
 		return a / b;
