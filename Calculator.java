d0f709a3206b47c2266819476f4108e20c399af6
himadri2092 <saffirehimadri@gmail.com>
2019-10-30 11:39:07 +0530

    text

---
+++ Calculator.java
@@ -0,0 +1,46 @@
+package com.himcode;
+
+
+public class Calculator implements ICalculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int subtraction(int a, int b) {
+		return a - b;
+	}
+
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+
+	public int divison(int a, int b) throws Exception {
+		if (b == 0) {
+			throw new Exception("Divider can't be zero.try something else which will not throw exception");
+		}
+
+		return a / b;
+	}
+
+	public boolean equalIntegers(int a, int b) {
+		boolean result = false;
+
+		if (a == b) {
+			result = true;
+			System.out.println("value is true");
+			System.out.println("value is true");
+			System.out.println("value is true");
+			System.out.println("value is true");
+		}
+
+		return result;
+return result;
+return result;
+return result;
+return result;
+
+
+
+	}
+}
